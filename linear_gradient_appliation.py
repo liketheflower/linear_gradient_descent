98c00761098083758ddb6b7fa14da3c12dd648d8
jimmy_shen <jim.morris.shen@gmail.com>
2020-04-30 23:14:09 -0400

    update

--- linear_gradient_appliation.py
+++ linear_gradient_appliation.py
@@ -1,19 +1,20 @@
 import numpy as np
 import random
-N = 1000
+import matplotlib.pyplot as plt
+N = 100
 x = np.random.randn(N)
 w, b = 2, 3
 
 y = w*x + b 
-z = np.random.normal(0, 0.004, N)
+z = np.random.normal(0, 0.1, N)
 y = y+z
 W, b = np.random.randn(), np.random.randn()
 
-epoches = 20
-batch_size = 16
+epoches = 30
+batch_size = 4
 
 batch_idx = list(range(N//batch_size+1))
-lr = 0.005
+lr = 0.0002
 batch_losses = []
 epoch_losses = []
 for epoch in range(epoches):
@@ -26,14 +27,17 @@ for epoch in range(epoches):
         y_hat = this_batch_x*w + b
         LOSS = np.sum(np.square(this_batch_y-y_hat))
         epoch_loss += LOSS
-        batch_lossed.append(LOSS)
+        batch_losses.append(LOSS)
         dL_dw = np.sum(2*this_batch_x*(y_hat-this_batch_y))
         dL_db = np.sum(2*(y_hat-this_batch_y))
         w -= lr*dL_dw
         b -= lr*dL_db
     print(epoch_loss)
     epoch_losses.append(epoch_loss)
-print(epoch_losses)
+#print(epoch_losses)
+plt.plot(range(len(batch_losses)), batch_losses)
+plt.savefig('loss.png')
+plt.show()
 print(w, b)
 
 
